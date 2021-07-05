d7042c2dbae5f4272043246b30fe3d859529bb8f
zeka2021 <gerasimovajeka@gmail.com>
2021-07-05 19:29:00 +0300

    create-js-hw-08-gallary

--- src/app.js
+++ src/app.js
@@ -63,3 +63,29 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+const galleryItemsContainer = document.querySelector('.js-gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryItemsContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+     <a
+       class="gallery__link"
+       href="${original}"
+      >
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="{$original}"
+      alt="${description}"
+     />
+    </a>
+  </li> 
+      `;
+    })
+    .join('');
+}
+
